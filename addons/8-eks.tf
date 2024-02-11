eb285fb790fe19f6ba19abc8eadeb24b2431bc79
David Driham <david.driham@gmail.com>
2024-02-11 23:00:51 +0200

    EKS

---
+++ addons/8-eks.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver/"
+  chart      = "aws-ebs-csi-driver"
+  namespace  = "kube-system"
+
+  set {
+    name  = "enableVolumeScheduling"
+    value = "true"
+  }
+
+  set {
+    name  = "enableVolumeResizing"
+    value = "true"
+  }
+
+  set {
+    name  = "enableVolumeSnapshot"
+    value = "true"
+  }
+}
